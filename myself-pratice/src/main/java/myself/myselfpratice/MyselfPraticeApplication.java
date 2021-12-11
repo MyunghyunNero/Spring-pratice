a9a0e677b7ef1f9767a4b16dea6e29f75b9af003
MyunghyungNero <mh1kim0910@gmail.com>
2021-12-11 14:46:32 +0900

    myself-pratice

---
+++ myself-pratice/src/main/java/myself/myselfpratice/MyselfPraticeApplication.java
@@ -0,0 +1,13 @@
+package myself.myselfpratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyselfPraticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyselfPraticeApplication.class, args);
+	}
+
+}
---
+++ myself-pratice/src/test/java/myself/myselfpratice/MyselfPraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package myself.myselfpratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyselfPraticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
